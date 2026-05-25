7eeee6f2045c9737ed653e482242884cd625095a
raf <24236329@siswa.um.edu.my>
2026-05-26 07:33:00 +0800

    feat: implement push method to log borrowed books

--- HistoryStack.java
+++ HistoryStack.java
@@ -2,13 +2,8 @@ import java.util.Stack;
 class HistoryStack {
     private Stack<BookNode> stack = new Stack<>();
 }
-import java.util.Stack;
 
-// Task 2 - Stack Structure
-class HistoryStack {
-    private Stack<BookNode> stack = new Stack<>();
-
-    // Adds book to the top of the stack
+// Adds book to the top of the stack
     public void push(BookNode b) {
         stack.push(b);
     }
