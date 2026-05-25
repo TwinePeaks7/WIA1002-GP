149abb146672be2a1aabe8012b39b43b0bad05be
ARShezan <24224813@siswa.um.edu.my>
2026-05-26 01:20:15 +0800

    BookNode class problem solved

--- BookNode_T3.java
+++ BookNode_T3.java
@@ -5,10 +5,6 @@ public class BookNode_T3 {
     public BookNode_T3 left;
     public BookNode_T3 right;
 
-     public BookNode_T3(Book book) {
-        this.book = book;
-        this.left = null;
-        this.right = null;
-    }
+
     
 }
