be007451e31fb642cdf7ce0e51c18b86c0c454a5
ARShezan <cspsejan@gmail.com>
2026-05-26 01:10:40 +0800

    BookNode class Successfully Implemented and completed

--- BookNode_T3.java
+++ BookNode_T3.java
@@ -5,5 +5,10 @@ public class BookNode_T3 {
     public BookNode_T3 left;
     public BookNode_T3 right;
 
+     public BookNode_T3(Book book) {
+        this.book = book;
+        this.left = null;
+        this.right = null;
+    }
     
 }
