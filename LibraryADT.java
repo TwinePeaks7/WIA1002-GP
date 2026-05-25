4e54f48e7f0604f60406c1ee25014ebad1a6b422
wangliming-2025 <24201317@siswa.um.edu.my>
2026-05-25 21:57:28 +0800

    Add files via upload

---
+++ LibraryADT.java
@@ -0,0 +1,8 @@
+package smartlibrary;
+public interface LibraryADT {
+    void addBook(int isbn,String title,String author);
+    void searchBook(int isbn);
+    void borrowBook(int isbn);
+    void viewLatestHistory();
+    void Menu();
+}
