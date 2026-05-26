140127f55956c45cefde4047bbbff309636b89b0
twine <ryanshaquille132@gmail.com>
2026-05-26 18:41:15 +0800

    feat: updated

---
+++ smartlibrary/BookNode_T3.java
@@ -0,0 +1,14 @@
+package smartlibrary;
+
+public class BookNode_T3 {
+
+    public Book book;
+    public BookNode_T3 left;
+    public BookNode_T3 right;
+
+    public BookNode_T3(Book book) {
+        this.book = book;
+        this.left = null;
+        this.right = null;
+    }
+}
