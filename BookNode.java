331f57f66a6121560942d8d36037f9157fd5814e
twine <ryanshaquille132@gmail.com>
2026-05-27 19:32:54 +0800

    added two updated files

---
+++ BookNode.java
@@ -0,0 +1,18 @@
+ package smartlibrary;
+// File: BookNode.java
+public class BookNode {
+    String isbn;
+    String title;
+    String author;
+    BookNode left;
+    BookNode right;
+
+    // Constructor to create a new book node
+    public BookNode(String isbn, String title, String author) {
+        this.isbn = isbn;
+        this.title = title;
+        this.author = author;
+        this.left = null;
+        this.right = null;
+    }
+}
